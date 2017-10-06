a641b16a47c75ca5836511ec8b10f01f8569f697
Ahmet Katrancı <ahmet@katranci.co.uk>
2017-10-06 21:03:07 +0300

    Add tests for the main run function

--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ import getStats from './get-stats/get-stats'
 import renderStats from './render-stats'
 
 const run = async () => {
-  const { owner, repo } = getRepoInfo()
+  const { owner, repo } = getRepoInfo() || {}
+  if (!owner) return
+
   const packageName = await getPackageName(owner, repo)
+  if (!packageName) return
+
   const stats = await getStats(packageName)
+  if (!stats) return
+
   renderStats(packageName, stats)
 }
 
-run()
+if (!process || !process.env || process.env.NODE_ENV !== 'test') {
+  run()
+}
+
+export default run
