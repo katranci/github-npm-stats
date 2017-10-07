28a2d59fff18d41cd13a76cd70bc08ebc7566688
Ahmet Katrancı <ahmet@katranci.co.uk>
2017-10-07 11:20:39 +0300

    Cover GitHub api 403 response

--- src/get-package-name/fetch-package-name.js
+++ src/get-package-name/fetch-package-name.js
@@ -1,6 +1,7 @@
 const fetchPackageName = (owner, repo) => {
   return fetch(`https://api.github.com/repos/${owner}/${repo}/contents/package.json`)
     .then(response => {
+      if (response.status === 403) throw new Error('Hourly GitHub api rate limit exceeded')
       if (response.status === 404) throw new Error('package.json is not found')
       return response.json()
     })
