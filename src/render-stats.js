5f95eebdc896229566d8502e328d09aadbded227
Ahmet Katranci <ahmet@katranci.co.uk>
2019-12-16 10:55:54 +0300

    Fix displaying popover chart on the new GitHub page structure

--- src/render-stats.js
+++ src/render-stats.js
@@ -47,36 +47,33 @@ const renderStats = (packageName, stats) => {
   const li = document.createElement('li')
   li.className = 'npm-stats'
   li.innerHTML = `
-    <div class="select-menu js-menu-container js-select-menu">
-      <a href="https://www.npmjs.com/package/${packageName}" target="_blank" class="btn btn-sm btn-with-count" title="View package on npmjs.com" aria-label="View package on npmjs.com">
-        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" height="13px" viewBox="0 0 18 7">
-          <path fill="#CB3837" d="M0,0h18v6H9v1H5V6H0V0z M1,5h2V2h1v3h1V1H1V5z M6,1v5h2V5h2V1H6z M8,2h1v2H8V2z M11,1v4h2V2h1v3h1V2h1v3h1V1H11z"/>
-          <polygon fill="#FFFFFF" points="1,5 3,5 3,2 4,2 4,5 5,5 5,1 1,1 "/>
-          <path fill="#FFFFFF" d="M6,1v5h2V5h2V1H6z M9,4H8V2h1V4z"/>
-          <polygon fill="#FFFFFF" points="11,1 11,5 13,5 13,2 14,2 14,5 15,5 15,2 16,2 16,5 17,5 17,1 "/>
-        </svg>
-      </a>
-      <span class="social-count js-menu-target">
+    <a href="https://www.npmjs.com/package/${packageName}" target="_blank" class="btn btn-sm btn-with-count" title="View package on npmjs.com" aria-label="View package on npmjs.com">
+      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" height="13px" viewBox="0 0 18 7">
+        <path fill="#CB3837" d="M0,0h18v6H9v1H5V6H0V0z M1,5h2V2h1v3h1V1H1V5z M6,1v5h2V5h2V1H6z M8,2h1v2H8V2z M11,1v4h2V2h1v3h1V2h1v3h1V1H11z"/>
+        <polygon fill="#FFFFFF" points="1,5 3,5 3,2 4,2 4,5 5,5 5,1 1,1 "/>
+        <path fill="#FFFFFF" d="M6,1v5h2V5h2V1H6z M9,4H8V2h1V4z"/>
+        <polygon fill="#FFFFFF" points="11,1 11,5 13,5 13,2 14,2 14,5 15,5 15,2 16,2 16,5 17,5 17,1 "/>
+      </svg>
+    </a>
+    <details class="details-reset details-overlay select-menu float-left">
+      <summary class="social-count select-menu-button" aria-haspopup="menu" role="button" aria-label="Toggle npm stats menu">
         ${stats.lastDay.toLocaleString()}
-        <span role="button" class="select-menu-button" aria-haspopup="true" aria-expanded="false" aria-label="Toggle npm stats menu"></span>
-      </span>
-      <div class="select-menu-modal-holder">
-        <div class="select-menu-modal js-menu-content">
-          <div class="select-menu-header">
-            <span class="select-menu-title">Downloads</span>
-          </div>
-          <dl>
-            <dt>Last day</dt>
-            <dd>${stats.lastDay.toLocaleString()}</dd>
-            <dt>Last week</dt>
-            <dd>${stats.lastWeek.toLocaleString()}</dd>
-            <dt>Last month</dt>
-            <dd>${stats.lastMonth.toLocaleString()}</dd>
-          </dl>
-          <canvas id="npm-stats-chart"></canvas>
+      </summary>
+      <details-menu class="select-menu-modal position-absolute mt-5">
+        <div class="select-menu-header">
+          <span class="select-menu-title">Downloads</span>
         </div>
-      </div>
-    </div>
+        <dl>
+          <dt>Last day</dt>
+          <dd>${stats.lastDay.toLocaleString()}</dd>
+          <dt>Last week</dt>
+          <dd>${stats.lastWeek.toLocaleString()}</dd>
+          <dt>Last month</dt>
+          <dd>${stats.lastMonth.toLocaleString()}</dd>
+        </dl>
+        <canvas id="npm-stats-chart"></canvas>
+      </details-menu>
+    </details>
   `
   pageheadActions.appendChild(li)
 
